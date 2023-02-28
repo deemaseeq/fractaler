ed98794e472fd74ff60499a6ecf257400e99020b
Dmytro Herman <dmitriy.german38@outlook.com>
2023-03-01 00:47:50 +0100

    Implemented recursion limit by predefined fractal depth.

--- SierpinskyTriangleShapes/ViewModels/MainWindowViewModel.cs
+++ SierpinskyTriangleShapes/ViewModels/MainWindowViewModel.cs
@@ -11,68 +11,50 @@ namespace SierpinskyTriangleShapes.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-	public MainWindowViewModel()
-	{
+    public MainWindowViewModel()
+    {
+        MainTriangle = new Triangle(500, new Point(0, 500));
+        MainTriangle.FillColor = Brushes.DarkSalmon;
 
-		//TODO Find a proper ObservableCollection implementation
-		TrianglesCollection = new ObservableCollection<Triangle>();
+        Depth = 6;
 
-		MainTriangle = new Triangle(500, new Point(0, 500));
-		MainTriangle.FillColor = Brushes.DarkSalmon;
+        TrianglesCollection = new ObservableCollection<Triangle>();
+        TrianglesCollection.Add(MainTriangle);
+
+        CutTheCenter(MainTriangle.Points, Depth);
+    }
+
+    public Triangle MainTriangle { get; private set; }
+    public ObservableCollection<Triangle> TrianglesCollection { get; private set; }
+    public int Depth { get; private set; }
 
 
+    private void CutTheCenter(IList<Point> basic, int depth)
+    {
+        if (depth == 0)
+        {
+            return;
+        }
 
-		TrianglesCollection.Add(MainTriangle);
+        Triangle cutted = Triangle.CreateInlineTriangle(basic);
+        cutted.FillColor = Brushes.WhiteSmoke;
+        TrianglesCollection.Add(cutted);
 
-		CutTheCenter(MainTriangle.Points);
-	}
+        CutTheCenter(new List<Point> { cutted.Points[0], basic[1], cutted.Points[1] }, depth - 1);
+        CutTheCenter(new List<Point> { basic[0], cutted.Points[0], cutted.Points[2] }, depth - 1);
+        CutTheCenter(new List<Point> { cutted.Points[2], cutted.Points[1], basic[2] }, depth - 1);
+    }
 
-	public Triangle MainTriangle { get; set; }
+    //TODO Find a way to use .xaml file extension instead of .axaml
 
-	private ObservableCollection<Triangle> trianglesCollection;
-	public ObservableCollection<Triangle> TrianglesCollection
-	{
-		get => trianglesCollection;
-		set
-		{
-			trianglesCollection = value;
-			//TODO Notify property changed
-		}
-	}
+    //TODO Define the structure of the VM
+    //TODO Find out how ICommand works with ReactiveUI and Avalonia
 
-	private int depth;
-	
+    //TODO Create a UI wrap for the process
+    //TODO We need a button to create a canvas and build the triangle with given depth
+    //TODO Implement rebuilding with other parameters
+    //TODO Implement a color change
 
-	private void CutTheCenter(IList<Point> basic)
-	{
-
-		Triangle cutted = Triangle.CreateInlineTriangle(basic);
-		cutted.FillColor = Brushes.WhiteSmoke;
-		TrianglesCollection.Add(cutted);
-
-		//! We can use some sort of tree structure to determine the depth of the fractal
-		//TODO Implement or find a suitable tree
-
-		if (cutted.SideLength < 5)
-		{
-			return;
-		}
-
-		CutTheCenter(new List<Point> { cutted.Points[0], basic[1], cutted.Points[1] });
-		CutTheCenter(new List<Point> { basic[0], cutted.Points[0], cutted.Points[2] });
-		CutTheCenter(new List<Point> { cutted.Points[2], cutted.Points[1], basic[2] });
-	}
-
-	//TODO Find a way to use .xaml file extension instead of .axaml
-
-	//TODO Define the structure of the VM
-	//TODO Find out how ICommand works with ReactiveUI and Avalonia
-
-	//TODO Create a UI wrap for the process
-	//TODO We need a button to create a canvas and build the triangle with given depth
-	//TODO Implement rebuilding with other parameters
-	//TODO Implement a color change
-
-	//TODO Determine if we need unit tests and write them if so
+    //TODO Determine if we need unit tests and write them if so
 }
 
